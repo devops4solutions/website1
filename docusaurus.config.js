8b4a61d5d7ea0e125255caeb4033c903cefa87a9
DevOps4Solutions <39961002+devops4solutions@users.noreply.github.com>
2022-11-01 14:35:04 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,7 +3,7 @@ module.exports = {
   title: 'Devops4Solutions Documentation',
   tagline: 'Documentation provided by Devops4solutions',
   url: 'https://devops4solutions.github.io',
-  baseUrl: '/docs',
+  baseUrl: '/docs/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
